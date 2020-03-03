5a56510bbea63392cac278d36d140aeb9150ebda
Trevor Riles <tjriles@spscommerce.com>
2020-03-03 15:14:38 -0600

    add docker image building

--- default.nix
+++ default.nix
@@ -1,9 +1,17 @@
+{ system ? builtins.currentSystem, maxLayers ? 20 }:
 let
   sources = import ./nix/sources.nix;
   compilerVersion = "ghc865";
-  pkgs = (import sources.nixpkgs) (import sources."haskell.nix");
-in
-pkgs.haskell-nix.cabalProject {
-  src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; };
-  ghc = pkgs.buildPackages.pkgs.haskell-nix.compiler.${compilerVersion};
+  pkgs = import sources.nixpkgs { overlays = (import sources."haskell.nix").overlays; inherit system;};
+  app = pkgs.haskell-nix.cabalProject {
+    src = pkgs.haskell-nix.haskellLib.cleanGit { src = ./.; };
+    ghc = pkgs.buildPackages.pkgs.haskell-nix.compiler.${compilerVersion};
+  };
+in pkgs.dockerTools.buildLayeredImage {
+  name = "app";
+  config.Cmd = [ "${app}/bin/app" ];
+  inherit maxLayers;
+  contents = [
+    app
+  ];
 }
